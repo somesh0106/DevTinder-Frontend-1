bb462d6e8e603e2664b90dfa641eca55d3e2f9ed
Nimesh Dwivedi <nimesh.dwivedi@gmail.com>
2025-06-26 06:27:44 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { BrowserRouter, Route,Routes } from "react-router-dom"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./profile"
+import Signup from "./signup"
+
 
 function App() {
   
 
   return (
     <>
+     <BrowserRouter basename="/">
+     <Routes>
+     <Route path="/" element={<Body/>}>
+
+     <Route path="/Login" element={<Login/>}/>
+<Route path="/profile" element= {<Profile/>}/>
+
+
+<Route path="/signup" element={<Signup/>}/>
+
+
+
+     </Route>
+
+
+
+
+     </Routes>
+     </BrowserRouter>
+
+
+
+   
       
-      <h1>Hello World</h1>
-     
     </>
   )
 }
